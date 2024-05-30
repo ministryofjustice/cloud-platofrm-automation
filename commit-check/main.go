a60b17a1dc619ff8c37c16c8a99bc1ec4d31342b
jackstockley89 <jstock8904@gmail.com>
2024-05-30 14:16:53 +0100

    feat(go): add outputs

--- commit-check/main.go
+++ commit-check/main.go
@@ -5,18 +5,29 @@ import (
 	"ministryofjustice/cloud-platform-automation/utils"
 	"os"
 	"strconv"
+	"strings"
+
+	"github.com/sethvargo/go-githubactions"
 )
 
 var (
-	owner    = os.Getenv("GITHUB_REPOSITORY_OWNER")
-	repo     = os.Getenv("GITHUB_REPOSITORY")
-	prnum, _ = strconv.Atoi(os.Getenv("PULL_REQUEST_NUMBER"))
-	file     = os.Getenv("FILE_NAME")
+	ref  = os.Getenv("GITHUB_REF")
+	repo = os.Getenv("GITHUB_REPOSITORY")
+	file = os.Getenv("FILE_NAME")
 )
 
 // main function to check if the pull request contains the file with the name specified and no other commited files.
 func main() {
-	f, _, err := utils.GetPullRequestFiles(owner, repo, prnum)
+
+	githubrefS := strings.Split(ref, "/")
+	prnum := githubrefS[2]
+	pull, _ := strconv.Atoi(prnum)
+
+	repoS := strings.Split(repo, "/")
+	owner := repoS[0]
+	repoName := repoS[1]
+
+	f, _, err := utils.GetPullRequestFiles(owner, repoName, pull)
 	if err != nil {
 		panic(err)
 	}
@@ -24,10 +35,10 @@ func main() {
 	b := utils.Files(file, f)
 	if b {
 		fmt.Printf("pull request commit contains the file with the name %v and no other commited files. This is valid for auto-approval.", file)
-		fmt.Println("::set-output name=approval::true")
+		githubactions.New().SetOutput("approval", "approval_not_needed")
 	}
 	if !b {
 		fmt.Printf("pull request commit does not contain the file with the name %v or contains other commited files. This is not valid for auto-approval.", file)
-		fmt.Println("::set-output name=approval::false")
+		githubactions.New().SetOutput("approval", "approval_needed")
 	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module ministryofjustice/cloud-platform-automation
 
 go 1.22.1
 
-require github.com/google/go-github/v61 v61.0.0
+require (
+	github.com/google/go-github/v61 v61.0.0
+	github.com/sethvargo/go-githubactions v1.2.0
+)
 
 require github.com/google/go-querystring v1.1.0 // indirect
